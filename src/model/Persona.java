bf3bfd4f42ba5a2835540d73e058eb63d9b0c550
juan-Riapira <juan.fernandez07@uptc.edu.co>
2023-11-18 12:13:22 -0500

    erorr

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -3,15 +3,26 @@ package model;
 public class Persona {
     private String name;
     private int edad;
-    private final int BONUS = 12;
+    private int sueldo;
 
     public Persona(String name, int edad) {
         this.name = name;
         this.edad = edad;
     }
 
-    public Persona(String name, int edad) {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
         this.edad = edad;
     }
 
