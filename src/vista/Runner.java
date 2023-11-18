e633a036f60c07bfeafd8c273832b6081aff2184
juan-Riapira <juan.fernandez07@uptc.edu.co>
2023-11-18 12:38:31 -0500

    cambios runner

--- src/vista/Runner.java
+++ src/vista/Runner.java
@@ -6,7 +6,9 @@ public class Runner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("ingrese el nombre ");
+        System.out.println("ingrese el nombre juan david  ");
         String nombre = sc.nextLine();
+        System.out.println("juan ");
+        System.out.println("juan");
     }
 }
